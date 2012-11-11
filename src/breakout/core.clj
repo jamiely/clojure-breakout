d409ed75e1ebdb35373f7adae8f09058b5ab42d7
Jamie Ly <me@jamie.ly>
2012-11-11 15:46:53 -0500

    Added movement for the paddle based on key presses

--- src/breakout/core.clj
+++ src/breakout/core.clj
@@ -1,9 +1,26 @@
 (ns breakout.core
+  (:use breakout.game)
   (:import
     (javax.swing JFrame)
     (java.awt Canvas Font Graphics Color Toolkit)
     (java.awt.event ActionListener KeyListener KeyEvent))
   (:gen-class :main true))
+
+;; The amount to move the paddle by
+(def paddle-offset (atom [0 0]))
+(def paddle-offset-multiplier (atom 40))
+
+(defn handle-input [#^KeyEvent event]
+  (condp = (.getKeyCode event)
+    KeyEvent/VK_LEFT (swap! paddle-offset #(map + [-1 0] %))
+    KeyEvent/VK_RIGHT (swap! paddle-offset #(map + [1 0] %))))
+
+(defn input-listener []
+  (proxy [ActionListener KeyListener] []
+    (actionPerformed [e])
+    (keyPressed [e] (handle-input e))
+    (keyReleased [e])
+    (keyTyped [e])))
 
 ;; got this draw method from the clojure tetris implementation
 (defn draw [#^Canvas canvas draw-fn]
@@ -36,6 +53,18 @@
     
     (draw-paddle paddle g)))
 
+;; Updates the location of the paddle
+(defn update-paddle [paddle offset]
+  (let [origin (:origin paddle)
+        x (:x origin)
+        y (:y origin)
+        multiplier @paddle-offset-multiplier
+        delta-x (* multiplier (nth @offset 0))
+        delta-y (* multiplier (nth @offset 1))
+        new-x (+ x delta-x)
+        new-y (+ y delta-y)]
+    (assoc paddle :origin {:x new-x :y new-y})))
+
 (defn -main [& args]
   (let [frame (JFrame. "Breakout")
         canvas (Canvas.)
@@ -48,25 +77,29 @@
     
     (doto canvas
       (.createBufferStrategy 2)
+      (.addKeyListener (input-listener))
       (.show)
       (.requestFocus))
 
     (loop [score 0
            old-time (System/currentTimeMillis)
            paddle paddle]
+      (reset! paddle-offset [0 0])
       (Thread/sleep 10)
+      
       (.setTitle frame (str "Breakout: " score))
       (let [blocks (atom nil)
             ball (atom nil)
             current-time (System/currentTimeMillis)
             new-time (long (if (> (- current-time old-time) 250)
                              current-time
-                             old-time))]
-        
-        (draw canvas (draw-game blocks paddle ball score))
+                             old-time))
+            updated-paddle (update-paddle paddle paddle-offset)]
+
+        (draw canvas (draw-game blocks updated-paddle ball score))
 
         :default
         (recur
          (+ score 1)
          new-time
-         paddle)))))
+         updated-paddle)))))
